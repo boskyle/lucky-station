36775275d7053bba6144e703112d22935025fdc6
boskyle <boskyle.orendain@gmail.com>
2021-10-09 13:32:08 -0400

    blog progress

--- lucky-station/src/routes/Blog/Blog.js
+++ lucky-station/src/routes/Blog/Blog.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import './Blog.scss';
+import luckyImg from '../../assets/images/Home/lucky_no_bg.png';
 
-const Blog = ({paragraph}) => {
+const Blog = ({paragraph,date}) => {
     return (
         <div className="content-wrap-blog">
-            <p>{paragraph}</p>
+            <small id="date-blog">{date}</small>
+            <p id="main-content-blog">{paragraph}</p>
+            <div className="blog-images-container border d-flex flex-column align-items-center">
+            <img src={luckyImg} className="w-50"></img>
+            </div>
+
         </div>
     )
 }
--- lucky-station/src/routes/Blog/BlogContainer.js
+++ lucky-station/src/routes/Blog/BlogContainer.js
@@ -5,7 +5,10 @@ import Blog from './Blog';
 const BlogContainer = () => {
     return (
         <section id="section-1-blog" className="grid-blog">
-            <Blog paragraph={'First entry. From the beginning of time I have fought against your innocence.'}/>
+            <Blog 
+            date={'Saturday,October 9, 2021'}
+            paragraph={'First entry. From the beginning of time I have fought against your innocence. I am puttng nonsense here bloop loop'}   
+            />
         </section>
     )
 }
